3b667cc2655b6333cd4cf1ff96bdaace92ec84db
McLarenCollege <hello@mclarencollege.in>
2019-10-29 08:46:37 +0530

    added UI

---
+++ lib/login_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Hero(
+                  tag: 'LogoImage',
+                  child: Image(
+                      image: NetworkImage(
+                          'https://mclarencollege.in/images/icon.png'),
+                      fit: BoxFit.contain),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'Login',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 36,
+                      color: Color(0xFF4790F1),
+                      fontFamily: 'Poppins'),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextField(
+                    textAlign: TextAlign.center,
+                    maxLines: 1,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        hintText: 'elon@musk.com',
+                        icon: Icon(Icons.email),
+                        border: OutlineInputBorder()),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    textAlign: TextAlign.center,
+                    maxLines: 1,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        icon: Icon(Icons.lock),
+                        hintText: 'spacexRocks',
+                        border: OutlineInputBorder()),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  RaisedButton(
+                    padding: EdgeInsets.all(16),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(12.0))),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white, fontSize: 21),
+                    ),
+                    color: Colors.blue,
+                    onPressed: () {
+                      Navigator.pushNamed(context, 'chat');
+                    },
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
+import 'package:chat_app_starter/register_screen.dart';
 import 'package:flutter/material.dart';
+import 'login_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: WelcomeScreen(),
-    )
+      initialRoute: '/',
+      routes: {
+        '/': (context) => WelcomeScreen(),
+        'login': (context) => LoginScreen(),
+        'register': (context) => RegisterScreen(),
+//        '/chat': (context) => ChatScreen(),
+      },
+    ),
   );
 }
-
 
 class WelcomeScreen extends StatefulWidget {
   @override
@@ -17,6 +24,71 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Hero(
+            tag: 'LogoImage',
+            child: Image(
+                image:
+                    NetworkImage('https://mclarencollege.in/images/icon.png'),
+                width: 200,
+                height: 200,
+                fit: BoxFit.contain),
+          ),
+          Container(
+            padding: EdgeInsets.all(12.0),
+            alignment: Alignment.center,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(
+                  'McLaren Chat',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 36,
+                      color: Color(0xFF4790F1),
+                      fontFamily: 'Poppins'),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'login');
+                  },
+                  padding: EdgeInsets.all(16),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0))),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white, fontSize: 21),
+                  ),
+                  color: Colors.blue,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'register');
+                  },
+                  padding: EdgeInsets.all(16),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0))),
+                  child: Text(
+                    'Register',
+                    style: TextStyle(color: Colors.white, fontSize: 21),
+                  ),
+                  color: Colors.purple,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
